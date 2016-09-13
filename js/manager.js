9008bc260015b7128f26bc16d5110b7a52106f06
nadine-shi <nadine@afterlogic.com>
2016-09-13 17:27:44 +0300

    System - use manager.js files instead of manager-smth.js

--- js/manager.js
+++ js/manager.js
@@ -1,23 +1,27 @@
 'use strict';
 
 module.exports = function (oAppData) {
-	var
-		_ = require('underscore'),
-		
-		TextUtils = require('%PathToCoreWebclientModule%/js/utils/Text.js'),
-		
-		Settings = require('modules/%ModuleName%/js/Settings.js'),
-		oSettings = _.extend({}, oAppData[Settings.ServerModuleName] || {}, oAppData['%ModuleName%'] || {})
-	;
+	var App = require('%PathToCoreWebclientModule%/js/App.js');
 	
-	Settings.init(oSettings);
+	if (App.getUserRole() === Enums.UserRole.NormalUser)
+	{
+		var
+			_ = require('underscore'),
+
+			TextUtils = require('%PathToCoreWebclientModule%/js/utils/Text.js'),
+
+			Settings = require('modules/%ModuleName%/js/Settings.js'),
+			oSettings = _.extend({}, oAppData[Settings.ServerModuleName] || {}, oAppData['%ModuleName%'] || {})
+		;
+
+		Settings.init(oSettings);
+
+		return {
+			start: function (ModulesManager) {
+				ModulesManager.run('SettingsWebclient', 'registerSettingsTab', [function () { return require('modules/%ModuleName%/js/views/OutlookSyncSettingsPaneView.js'); }, Settings.HashModuleName, TextUtils.i18n('%MODULENAME%/LABEL_SETTINGS_TAB')]);
+			}
+		};
+	}
 	
-	return {
-		isAvailable: function (iUserRole, bPublic) {
-			return !bPublic && iUserRole === Enums.UserRole.NormalUser;
-		},
-		start: function (ModulesManager) {
-			ModulesManager.run('SettingsWebclient', 'registerSettingsTab', [function () { return require('modules/%ModuleName%/js/views/OutlookSyncSettingsPaneView.js'); }, Settings.HashModuleName, TextUtils.i18n('%MODULENAME%/LABEL_SETTINGS_TAB')]);
-		}
-	};
+	return null;
 };
