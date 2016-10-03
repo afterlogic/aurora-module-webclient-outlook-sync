9922ab87a82878f0ca03401b10df00838663ace6
nadine-shi <nadine@afterlogic.com>
2016-10-03 09:15:10 +0300

    MobileSync, OutlookSync - display user account login in settings - test

--- js/views/OutlookSyncSettingsPaneView.js
+++ js/views/OutlookSyncSettingsPaneView.js
@@ -32,6 +32,8 @@ function COutlookSyncSettingsPaneView()
 	this.credentialsHintText = ko.computed(function () {
 		return TextUtils.i18n('COREWEBCLIENT/INFO_MOBILE_CREDENTIALS', {'LOGIN': App.userAccountLogin()});
 	}, this);
+		console.log('-- App.userAccountLogin()', App.userAccountLogin());
+		console.log('-- this.credentialsHintText', this.credentialsHintText());
 	this.credentialsHintText.subscribe(function () {
 		console.log('- App.userAccountLogin()', App.userAccountLogin());
 		console.log('- this.credentialsHintText', this.credentialsHintText());
