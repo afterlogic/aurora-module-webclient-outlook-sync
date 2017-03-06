d1af17ae3343ce6ef98d1a49d217908042dc9d9c
sash04ek <a.ovcharov@gmail.com>
2017-03-06 08:50:37 +0300

    refactoring namespaces

--- module.php
+++ module.php
@@ -1,8 +1,8 @@
 <?php
 
-namespace Aurora\Modules;
+namespace Aurora\Modules\OutlookSyncWebclient;
 
-class OutlookSyncWebclientModule extends \Aurora\System\Module\AbstractModule
+class Module extends \Aurora\System\Module\AbstractModule
 {
 	/***** public functions might be called with web API *****/
 	/**
